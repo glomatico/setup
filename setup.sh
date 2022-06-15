fabf84760f51333421ecfa2f4307c431aafafcb2
R. M <50295204+glomatico@users.noreply.github.com>
2022-06-15 16:18:50 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #Update
-apt update
+sudo apt update
 
 #Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -7,52 +7,52 @@ sudo apt-get install ./google-chrome-stable_current_amd64.deb
 
 #Java
 wget https://download.bell-sw.com/java/17.0.3.1+2/bellsoft-jdk17.0.3.1+2-linux-amd64-full.deb
-apt install ./bellsoft-jdk17.0.3.1+2-linux-amd64-full.deb
+sudo apt install ./bellsoft-jdk17.0.3.1+2-linux-amd64-full.deb
 
 #Google Chrome Guest
-wget https://raw.githubusercontent.com/glomatico/setup/main/google-chrome.desktop -O /usr/share/applications/google-chrome.desktop
+sudo wget https://raw.githubusercontent.com/glomatico/setup/main/google-chrome.desktop -O /usr/share/applications/google-chrome.desktop
 
 #VirtualBox
-apt install virtualbox
+sudo apt install virtualbox
 
 #VLC
-apt install vlc
+sudo apt install vlc
 
 #Thonny
-apt install thonny
+sudo apt install thonny
 
 #Libre Office
-apt install libreoffice
+sudo apt install libreoffice
 
 #Eclipse
 wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2022-03/R/eclipse-java-2022-03-R-linux-gtk-x86_64.tar.gz
 tar -zxvf eclipse-java-2022-03-R-linux-gtk-x86_64.tar.gz -C /opt/
 wget https://dl2.macupdate.com/images/icons128/11662.png -O /opt/eclipse/eclipse.png
-echo -e '[Desktop Entry]\n Version=1.0\n Name=Eclipse\n Exec=/opt/eclipse/eclipse\n Icon=/opt/eclipse/eclipse.png\n Type=Application\n Categories=Application'
+sudo echo -e '[Desktop Entry]\n Version=1.0\n Name=Eclipse\n Exec=/opt/eclipse/eclipse\n Icon=/opt/eclipse/eclipse.png\n Type=Application\n Categories=Application'
 
 #brModelo
-mkdir /opt/brmodelo
-wget https://github.com/chcandido/brModelo/releases/download/3.31/brModelo.jar -O /opt/brmodelo/brmodelo.jar
-wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O /opt/brmodelo/brmodelo.png
-echo -e '[Desktop Entry]\n Version=1.0\n Name=brModelo\n Exec=java -jar /opt/brmodelo/brmodelo.jar\n Icon=/opt/brmodelo/brmodelo.png\n Type=Application\n Categories=Application'
+sudo mkdir /opt/brmodelo
+sudo wget https://github.com/chcandido/brModelo/releases/download/3.31/brModelo.jar -O /opt/brmodelo/brmodelo.jar
+sudo wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O /opt/brmodelo/brmodelo.png
+sudo echo -e '[Desktop Entry]\n Version=1.0\n Name=brModelo\n Exec=java -jar /opt/brmodelo/brmodelo.jar\n Icon=/opt/brmodelo/brmodelo.png\n Type=Application\n Categories=Application'
 
 #PyCharm
-snap install pycharm-community --classic
+sudo snap install pycharm-community --classic
 
 #IntelliJ IDEA
-snap install intellij-idea-community --classic
+sudo snap install intellij-idea-community --classic
 
 #NetBeans
-snap install netbeans --classic
+sudo snap install netbeans --classic
 
 #MySQL Workbench
-snap install mysql-workbench-community
+sudo snap install mysql-workbench-community
 
 #Android Studio
-snap install android-studio --classic
+sudo snap install android-studio --classic
 
 #Visual Studio Code
-snap install code --classic
+sudo snap install code --classic
 
 #Postman
-snap install postman
+sudo snap install postman
