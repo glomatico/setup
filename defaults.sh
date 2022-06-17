3ae55343f34b266292b4853b872fc0c956917666
R. M <50295204+glomatico@users.noreply.github.com>
2022-06-17 09:37:50 -0300

    Create defaults.sh

---
+++ defaults.sh
@@ -0,0 +1,4 @@
+dconf reset -f /
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
+gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'eclipse.desktop']"
