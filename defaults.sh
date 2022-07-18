d0c71a549525c5919e68a9c58e49f418cdf4ddee
R. M <50295204+glomatico@users.noreply.github.com>
2022-07-18 07:45:28 -0300

    Update defaults.sh

--- defaults.sh
+++ defaults.sh
@@ -1,4 +1,3 @@
 dconf reset -f /
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-trash false
-gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'eclipse.desktop', 'org.gnome.Nautilus.desktop']"
