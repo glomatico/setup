e982d3d19b1e702060b0b2db68cc8c8bcd329ec7
R. M <50295204+glomatico@users.noreply.github.com>
2022-07-20 09:17:35 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir -p ~/.config/autostart
 wget raw.githubusercontent.com/glomatico/setup/main/defaults.desktop -O ~/.config/autostart/defaults.desktop
 
-wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O ~/brmodelo.png
+wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O ~/brModelo.png
 wget https://raw.githubusercontent.com/glomatico/setup/main/brmodelo.desktop -O ~/.local/share/applications/brmodelo.desktop
