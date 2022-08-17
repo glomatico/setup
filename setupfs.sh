57ceb1d7a2330bf80c65b8633152cdb3b900aeaa
R. M <50295204+glomatico@users.noreply.github.com>
2022-08-17 11:38:46 -0300

    Create setupfs.sh

---
+++ setupfs.sh
@@ -0,0 +1,82 @@
+#Update
+sudo apt update
+
+#Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+#Java
+wget https://download.bell-sw.com/java/17.0.3.1+2/bellsoft-jdk17.0.3.1+2-linux-amd64-full.deb
+sudo apt install -y ./bellsoft-jdk17.0.3.1+2-linux-amd64-full.deb
+
+#Google Chrome Guest
+sudo wget https://raw.githubusercontent.com/glomatico/setup/main/google-chrome.desktop -O /usr/share/applications/google-chrome.desktop
+
+#LibreOffice
+sudo apt install -y libreoffice
+
+#VirtualBox
+sudo apt install -y virtualbox
+
+#VLC
+sudo apt install -y vlc
+
+#Thonny
+sudo apt install -y thonny
+
+#Eclipse
+wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2022-06/R/eclipse-java-2022-06-R-linux-gtk-x86_64.tar.gz
+sudo tar -zxvf eclipse-java-2022-06-R-linux-gtk-x86_64.tar.gz -C /opt/
+sudo wget https://www.eclipse.org/downloads/assets/public/images/logo-eclipse.png -O /opt/eclipse/eclipse.png
+sudo wget https://raw.githubusercontent.com/glomatico/setup/main/eclipse.desktop -O /usr/share/applications/eclipse.desktop
+
+#brModelo
+sudo mkdir /opt/brmodelo
+sudo wget https://github.com/chcandido/brModelo/releases/download/3.31/brModelo.jar -O /opt/brmodelo/brmodelo.jar
+sudo wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O /opt/brmodelo/brmodelo.png
+sudo wget https://raw.githubusercontent.com/glomatico/setup/main/brmodelo.desktop -O /usr/share/applications/brmodelo.desktop
+
+#PyCharm
+sudo snap install pycharm-community --classic
+
+#IntelliJ IDEA
+sudo snap install intellij-idea-community --classic
+
+#NetBeans
+sudo snap install netbeans --classic
+
+#MySQL Workbench
+sudo snap install mysql-workbench-community
+
+#Android Studio
+sudo snap install android-studio --classic
+
+#Visual Studio Code
+sudo snap install code --classic
+
+#Postman
+sudo snap install postman
+
+#Create alunoinfo User
+sudo useradd -s /bin/bash -d /home/alunoinfo -m alunoinfo
+echo "alunoinfo:alunoinfo" | sudo chpasswd
+
+#Defaults
+sudo -u alunoinfo mkdir -p /home/alunoinfo/.config/autostart
+sudo wget https://raw.githubusercontent.com/glomatico/setup/main/defaultsfs.desktop -O /home/alunoinfo/.config/autostart/defaultsfs.desktop
+sudo chattr +i /home/alunoinfo/.config/autostart/defaultsfs.desktop
+
+#Remove Packages
+sudo apt remove -y gnome-initial-setup gnome-startup-applications
+
+#Upgrade
+sudo apt upgrade -y
+
+#Usermod Thonny
+sudo usermod -a -G dialout alunoinfo
+
+#Apt Post Script
+echo 'DPkg::Post-Invoke {"sudo wget https://raw.githubusercontent.com/glomatico/setup/main/google-chrome.desktop -O /usr/share/applications/google-chrome.desktop";};' | sudo tee -a /etc/apt/apt.conf > /dev/null
+
+#Poweroff
+sudo poweroff
