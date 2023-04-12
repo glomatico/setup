8fa31c9863d6e07c617cdbd35afab79da5559a52
R. M <50295204+glomatico@users.noreply.github.com>
2023-04-12 14:58:30 -0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.config/autostart
-wget raw.githubusercontent.com/glomatico/setup/main/defaults.desktop -O ~/.config/autostart/defaults.desktop
-
-wget https://github.com/chcandido/brModelo/raw/master/src/imagens/icone.png -O ~/brModelo.png
-wget https://raw.githubusercontent.com/glomatico/setup/main/brmodelo.desktop -O ~/.local/share/applications/brmodelo.desktop
-wget https://github.com/glomatico/setup/raw/main/firefox.tar.gz -O ~/snap/firefox.tar.gz
-rm -rf ~/snap/firefox
-tar -xvf ~/snap/firefox.tar.gz --directory ~/snap
-rm ~/snap/firefox.tar.gz
