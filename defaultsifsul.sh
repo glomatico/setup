8850edb09d95c80d824b2c5a31b4293235579456
R. M <50295204+glomatico@users.noreply.github.com>
2023-06-30 10:39:13 -0300

    Update defaultsifsul.sh

--- defaultsifsul.sh
+++ defaultsifsul.sh
@@ -1,1 +1,11 @@
-sh -c "dconf reset -f / && dconf write /org/gnome/desktop/input-sources/sources \"[('xkb', 'br')]\" && dconf write /org/gnome/shell/favorite-apps \"['firefox_firefox.desktop', 'eclipse.desktop', 'org.gnome.Nautilus.desktop']\" && dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false && dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false && dconf write /org/gnome/desktop/sound/event-sounds false && dconf write /org/gnome/desktop/interface/enable-animations false && dconf write /org/gnome/desktop/interface/gtk-theme '\"Yaru-olive\"' && dconf write /org/gnome/desktop/interface/icon-theme '\"Yaru-olive\"' && dconf write /org/gnome/desktop/background/picture-uri '\"/usr/share/backgrounds/ifsul.png\"'"
+/bin/cp /etc/skel/.bashrc ~/
+dconf reset -f /
+dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'br')]"
+dconf write /org/gnome/shell/favorite-apps "['firefox_firefox.desktop', 'eclipse.desktop', 'org.gnome.Nautilus.desktop']"
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
+dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false
+dconf write /org/gnome/desktop/sound/event-sounds false
+dconf write /org/gnome/desktop/interface/enable-animations false
+dconf write /org/gnome/desktop/interface/gtk-theme '"Yaru-olive"'
+dconf write /org/gnome/desktop/interface/icon-theme '"Yaru-olive"'
+dconf write /org/gnome/desktop/background/picture-uri '"/usr/share/backgrounds/ifsul.png"'
