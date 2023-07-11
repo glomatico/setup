2985ed0f2c09e142b8bbfce75324a58a2796f32e
R. M <50295204+glomatico@users.noreply.github.com>
2023-07-11 15:03:11 -0300

    Update defaultsifsul.sh

--- defaultsifsul.sh
+++ defaultsifsul.sh
@@ -8,9 +8,10 @@ rm -rf ~/Desktop/*
 rm -rf ~/Documents/*
 rm -rf ~/Downloads/*
 rm -rf ~/eclipse-workspace
+rm -rf ~/snap/eclipse
 dconf reset -f /
 dconf write /org/gnome/desktop/input-sources/sources "[('xkb', 'br')]"
-dconf write /org/gnome/shell/favorite-apps "['firefox_firefox.desktop', 'eclipse.desktop', 'org.gnome.Nautilus.desktop']"
+dconf write /org/gnome/shell/favorite-apps "['firefox_firefox.desktop', 'eclipse_eclipse.desktop', 'org.gnome.Nautilus.desktop']"
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-trash false
 dconf write /org/gnome/shell/extensions/dash-to-dock/show-mounts false
 dconf write /org/gnome/desktop/sound/event-sounds false
