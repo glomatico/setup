c3a733fb9667e35551d3546184c5df3d1ca5a113
R. M <50295204+glomatico@users.noreply.github.com>
2023-08-24 14:50:39 -0300

    Create alunoinfo_clean.bat

---
+++ alunoinfo_clean.bat
@@ -0,0 +1,14 @@
+:: Remove all files and folders from Downloads folder
+del /q "%userprofile%\Downloads\*"
+for /d %%p in ("%userprofile%\Downloads\*") do rmdir "%%p" /s /q
+
+:: Remove all files and folders from Desktop
+del /q "%userprofile%\Desktop\*"
+for /d %%p in ("%userprofile%\Desktop\*") do rmdir "%%p" /s /q
+
+:: Remove Roblox
+rmdir /s /q "%localappdata%\Roblox"
+rmdir /s /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Roblox"
+
+:: Remove Google Chrome user data
+rmdir /s /q "%localappdata%\Google\Chrome\User Data"
